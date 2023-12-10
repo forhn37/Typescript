b23114d4a20d4980d1388b6e71e29d883a0378ed
kimhyun <forhn37@gmail.com>
2023-12-10 16:40:16 +0900

    prac : boolean type check

--- chapter-2/src/test.ts
+++ chapter-2/src/test.ts
@@ -1,7 +1,6 @@
-let a : unknown = 30;
-let b = a === 123;
-let c = a + 10;
-if (typeof a ==='number') {
-  let d = a + 10
-  console.log(d)
-}
+let a = true;
+var b = false;
+const c = 5;
+let d: boolean = true;
+let e : true = true;
+let f : true = false;
