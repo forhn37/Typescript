958fc7ca3f984acb00d08b0fc390f7fa0ab6b5aa
kimhyun <forhn37@gmail.com>
2023-12-10 16:47:49 +0900

    prac : 숫자분리자 처리

--- chapter-2/dist/test.js
+++ chapter-2/dist/test.js
@@ -1,8 +1,12 @@
 "use strict";
-let a = 30;
-let b = a === 123;
-let c = a + 10;
-if (typeof a === 'number') {
-    let d = a + 10;
-    console.log(d);
-}
+let a = 1234;
+var b = Infinity * 0.10;
+const c = 5678;
+let d = a < b;
+let e = 100;
+let f = 26.218;
+let g = 10;
+
+
+let oneMillion = 1_000_000
+let twoMillion : 2_000_000 = 2_000_000;
--- chapter-2/src/test.ts
+++ chapter-2/src/test.ts
@@ -1,6 +1,10 @@
-let a = true;
-var b = false;
-const c = 5;
-let d: boolean = true;
-let e : true = true;
-let f : true = false;
+let a =1234;
+var b = Infinity * 0.10;
+const c =5678;
+let d = a < b
+let e: number = 100
+let f: 26.218 = 26.218;
+let g : 26.218 = 10
+
+let oneMillion = 1_000_000
+let twoMillion : 2_000_000 = 2_000_000;
